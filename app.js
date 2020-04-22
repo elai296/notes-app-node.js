2b4b6d34dd5b107dd81dea75380cf161d0af7cfd
Elaine Phan <elainep926@gmail.com>
2020-04-22 15:35:08 -0700

    math problem

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const fs = require('fs')
+const add = require('./utils.js')
 
 // fs.writeFileSynch('notes.txt', 'This file was created by Node.js')
 
-fs.appendFileSync('notes.txt', ' I live in California');
+// fs.appendFileSync('notes.txt', ' I live in California');
+
+const sum = add (4, -2)
+
+console.log(sum)
